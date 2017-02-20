6a82464b2a23d1ea9a61697397c4b2c555fc4ff0
ITLAB <ITLAB@IT208>
2017-02-20 13:59:09 +0530

    Implementation.

--- src/com/example/rwsd/MainActivity.java
+++ src/com/example/rwsd/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
@@ -12,24 +14,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        EditText fileName,content;
+        Button write,read;
+        
+        fileName=(EditText)findViewById(R.id.editText1);
+        content=(EditText)findViewById(R.id.editText2);
+        write=(Button)findViewById(R.id.button1);
+        read=(Button)findViewById(R.id.button2);
+        
+        
+        		
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    
 }
